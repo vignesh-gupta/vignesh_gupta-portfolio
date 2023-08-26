b63c2a1082a04d2990429da62bebfc9cc7513850
Vignesh <vighneshgupta32@gmail.com>
2023-08-26 10:12:58 +0530

    [updated] layout

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,25 @@
+import clsx from "clsx";
+import { Preahvihear } from "next/font/google";
+import Navbar from "@/components/sections/Navbar";
+import Footer from "@/components/sections/Footer";
+
+const preahvihear = Preahvihear({ subsets: ["latin"], weight: ["400"] });
+
+const HomeLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div
+      className={clsx(
+        "w-full bg-primary text-foreground",
+        preahvihear.className
+      )}
+    >
+      <Navbar />
+      <main className="xl:px-80 lg:px-48 md:px-32 sm:px-16 px-10">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default HomeLayout;
