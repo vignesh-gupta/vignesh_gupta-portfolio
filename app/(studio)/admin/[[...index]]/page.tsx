f510bf7e978792b86edf7b5543fbc597b93a31f2
Vignesh Gupta <vighneshgupta32@gmail.com>
2023-10-04 20:06:03 +0530

    [fix] next studio issue + footer styling

--- app/(studio)/admin/[[...index]]/page.tsx
+++ app/(studio)/admin/[[...index]]/page.tsx
@@ -4,5 +4,9 @@ import { NextStudio } from 'next-sanity/studio';
 import config from '@/sanity.config';
 
 export default function AdminPage() {
-  return <NextStudio config={config} />;
+  return (
+    <body>
+      <NextStudio config={config} />
+    </body>
+  );
 }
--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -3,7 +3,7 @@ import { BsLinkedin, BsGithub } from 'react-icons/bs';
 
 const FooterSection = () => {
   return (
-    <footer className='flex h-20 justify-between border-t border-secondary px-10 py-3 sm:px-16 md:px-32 lg:px-48 xl:px-80'>
+    <footer className='flex h-20 items-center justify-between border-t border-secondary px-10 py-3 sm:px-16 md:px-32 lg:px-48 xl:px-80'>
       <p>
         &copy; {new Date().getFullYear()} Vignesh Gupta. Checkout on
         <a
